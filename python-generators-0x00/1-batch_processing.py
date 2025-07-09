d7042e1f92c6d764a047bc1f77c39e6d9693e89d
EgehizaJoy <egehizafuraha@gmail.com>
2025-07-09 16:44:05 +0300

    Add batch processing generator to stream and filter users over 25

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from decimal import Decimal
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ALX_prodev",
+        port=3307
+    )
+    cursor = connection.cursor(dictionary=True)
+    try:
+        cursor.execute("SELECT * FROM user_data")
+        batch = []
+        for row in cursor:
+            if isinstance(row["age"], Decimal):
+                row["age"] = int(row["age"])
+            batch.append(row)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        if batch:
+            yield batch
+    finally:
+        cursor.close()
+        connection.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user["age"] > 25:
+                print(user)
