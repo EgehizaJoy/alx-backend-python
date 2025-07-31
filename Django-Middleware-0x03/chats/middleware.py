bb68fa47f9c27c19932257c56e74d4e8cf92699c
EgehizaJoy <egehizafuraha@gmail.com>
2025-07-31 16:40:05 +0300

    added time log

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 # middleware.py
 import logging
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 logger = logging.getLogger(__name__)
 
@@ -11,4 +12,14 @@ class RequestLoggingMiddleware:
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else "Anonymous"
         logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
+        return self.get_response(request)
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        # Allow access only between 6 PM (18) and 9 PM (21), inclusive
+        if current_hour < 18 or current_hour > 21:
+            return HttpResponseForbidden("Access denied: Outside allowed hours (6PM–9PM)")
         return self.get_response(request)
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'messaging_app.middleware.RestrictAccessByTimeMiddleware',
     'messaging_app.middleware.RequestLoggingMiddleware',
 ]
 
