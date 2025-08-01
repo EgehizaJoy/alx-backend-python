05368abe5540bd02857a340e7963ef4780c6a134
EgehizaJoy <egehizafuraha@gmail.com>
2025-08-01 16:04:04 +0300

    messaging app

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from .models import Message, Notification
 
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('sender', 'receiver', 'timestamp')
-    search_fields = ('sender__username', 'receiver__username', 'content')
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'message', 'created_at', 'is_read')
-    list_filter = ('is_read',)
-    search_fields = ('user__username',)
+# Register your models here.
