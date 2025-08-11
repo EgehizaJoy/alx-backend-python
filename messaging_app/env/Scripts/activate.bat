e61a52e801e93c123cd0689a2de3daa464023feb
EgehizaJoy <egehizafuraha@gmail.com>
2025-08-11 16:11:22 +0300

    docker image

--- messaging_app/env/Scripts/activate.bat
+++ messaging_app/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\messaging_app\env
+set VIRTUAL_ENV=C:\Users\Hp\alx-backend-python\messaging_app\env
 
 if not defined PROMPT set PROMPT=$P$G
 
